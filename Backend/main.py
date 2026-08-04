f9d67bb10f9c5075b767a8f87c6995f506ea4515
indri-esti <indriesti58@gmail.com>
2026-08-04 13:40:55 +0700

    Perbaikan backend

--- Backend/main.py
+++ Backend/main.py
@@ -1,21 +1,35 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 from ai import balas
 from memory import load_memory, save_memory
 
+app = Flask(__name__)
+CORS(app)
+
 memory = load_memory()
 
-print("=== AI.Ind ===")
 
-while True:
+@app.route("/")
+def home():
+    return {
+        "status": "AI.Ind Backend Running"
+    }
 
-    pesan = input("Kamu : ")
 
-    if pesan.lower() == "keluar":
-        print("AI : Sampai jumpa!")
-        break
+@app.route("/chat", methods=["POST"])
+def chat():
+
+    data = request.get_json()
+
+    pesan = data.get("message", "").strip()
+
+    if pesan == "":
+        return jsonify({
+            "reply": "Pesan tidak boleh kosong."
+        }), 400
 
     jawaban = balas(pesan)
-
-    print("\nAI :", jawaban)
 
     memory.append({
         "kamu": pesan,
@@ -23,3 +37,15 @@ while True:
     })
 
     save_memory(memory)
+
+    return jsonify({
+        "reply": jawaban
+    })
+
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+    )
