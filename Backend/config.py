262669b48572f41548efc4745314c6cf402519bd
indri-esti <indriesti85@gmail.com>
2026-08-07 18:09:48 +0700

    Fix load env for PythonAnywhere

--- Backend/config.py
+++ Backend/config.py
@@ -1,12 +1,17 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
 
 API_KEY = os.getenv("OPENROUTER_API_KEY")
 
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 
+
 BASE_URL = "https://openrouter.ai/api/v1/chat/completions"
+
 
 MODEL = "openrouter/free"
