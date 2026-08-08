c4aeb3c89f96b4ae96506ded08fa92fa31a02214
indri-esti <indriesti85@gmail.com>
2026-08-08 12:01:24 +0700

    fix Android build by disabling PWA

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,31 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-
-    VitePWA({
-      registerType: "autoUpdate",
-
-      manifest: {
-        name: "AI.Ind",
-        short_name: "AI.Ind",
-        description: "AI Buatan Indonesia",
-        theme_color: "#081420",
-        background_color: "#081420",
-        display: "standalone",
-
-        icons: [
-          {
-            src: "/logo.svg",
-            sizes: "512x512",
-            type: "image/svg+xml",
-          },
-        ],
-      },
-    }),
   ],
 })
